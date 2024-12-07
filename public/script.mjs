26045b6a58ee29df14b7643ebed23a33aae75401
Rafael Torok <rafaeltorok@gmail.com>
2024-12-07 14:40:18 -0300

    Added input fields to "calculate specs" Removed the page prompt and added proper input fields to improve the UI

--- public/script.mjs
+++ public/script.mjs
@@ -132,6 +132,72 @@ function displayGPUs() {
     }
 }
 
+function calculatePerformance() {
+    const dataDisplay = document.getElementById("dataDisplay");
+    const gpuDataDisplay = document.getElementById("gpuDataDisplay");
+
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const gpu = getFromDatabase(searchInput);
+
+    if (gpu) {
+        gpuDataDisplay.innerHTML = "";  // Removes any previous GPU table
+        let coreClockInputField = document.getElementById("coreClockInputField");
+        let memClockInputField = document.getElementById("memClockInputField");
+        let clockFieldsButton = document.getElementById("clockFieldsButton");
+
+        if (!coreClockInputField || !memClockInputField || !clockFieldsButton) {
+            coreClockInputField = document.createElement("input");
+            memClockInputField = document.createElement("input");
+            clockFieldsButton = document.createElement("button");
+            
+            coreClockInputField.id = "coreClockInputField";
+            memClockInputField.id = "memClockInputField";
+            clockFieldsButton.id = "clockFieldsButton";
+
+            coreClockInputField.setAttribute("placeholder", "Core clock (in MHz)");
+            memClockInputField.setAttribute("placeholder", "Memory clock (in Gbps)");
+            clockFieldsButton.textContent = "Calculate";
+
+            dataDisplay.appendChild(coreClockInputField);
+            dataDisplay.appendChild(memClockInputField);
+            dataDisplay.appendChild(clockFieldsButton);
+        }
+
+        clockFieldsButton.disabled = false;  // If the GPU was found, allows the user to use this button
+        dataDisplay.appendChild(gpuDataDisplay);
+        const gpuContainer = createGPUContainer(gpu, gpu.getBoostClock(), gpu.getMemClock());
+        gpuDataDisplay.appendChild(gpuContainer);
+
+        clockFieldsButton.addEventListener("click", () => {
+            gpuDataDisplay.innerHTML = "";  // Removes the previous GPU table to create a new one with the updated values
+            let clockSpeed = parseInt(coreClockInputField.value)
+            let memClock = parseFloat(memClockInputField.value);
+
+            // Validate that the inputs are valid numbers, if not, use the default values
+            if (isNaN(clockSpeed) || 
+            clockSpeed === null || 
+            clockSpeed <= 0) {
+                clockSpeed = gpu.getBoostClock();
+            }
+
+            if (isNaN(memClock) || 
+            memClock === null || 
+            memClock <= 0) {
+                memClock = gpu.getMemClock();
+            }
+
+            const gpuContainer = createGPUContainer(gpu, clockSpeed, memClock);
+            gpuDataDisplay.appendChild(gpuContainer);
+        });
+    } else {
+        gpuDataDisplay.innerHTML = "";  // Clears the screen before displaying the message
+        const message = document.createElement("h2");
+        message.textContent = "GPU was not found on the database. Please use the \"Add GPU\" button";
+        gpuDataDisplay.appendChild(message);
+        clockFieldsButton.disabled = true;
+    }
+}
+
 function showSearchBoxCalcPerf() {
     clearScreen();
     const dataDisplay = document.getElementById("dataDisplay");
@@ -152,41 +218,6 @@ function showSearchBoxCalcPerf() {
     searchBox.appendChild(searchInput);
     searchBox.appendChild(searchButton);
     dataDisplay.appendChild(searchBox);
-    dataDisplay.appendChild(gpuDataDisplay);
-}
-
-function calculatePerformance() {
-    const dataDisplay = document.getElementById("dataDisplay");
-    const gpuDataDisplay = document.getElementById("gpuDataDisplay");
-    gpuDataDisplay.innerHTML = "";  // Removes the previous GPU table before displaying a new one
-
-    const searchInput = document.getElementById('searchInput').value.toLowerCase();
-    const gpu = getFromDatabase(searchInput);
-
-    if (gpu) {
-        let clockSpeed = parseInt(prompt("Enter the core clock (in MHz)"))
-        let memClock = parseFloat(prompt("Enter the memory speed (in GHz)"));
-
-        // Validate that the inputs are valid numbers, if not, use the default values
-        if (isNaN(clockSpeed) || 
-        clockSpeed === null || 
-        clockSpeed < 0) {
-            clockSpeed = gpu.getBoostClock();
-        }
-
-        if (isNaN(memClock) || 
-        memClock === null || 
-        memClock < 0) {
-            memClock = gpu.getMemClock();
-        }
-
-        const gpuContainer = createGPUContainer(gpu, clockSpeed, memClock);
-        gpuDataDisplay.appendChild(gpuContainer);
-    } else {
-        const message = document.createElement("h2");
-        message.textContent = "GPU was not found on the database. Please use the \"Add GPU\" button";
-        gpuDataDisplay.appendChild(message);
-    }
     dataDisplay.appendChild(gpuDataDisplay);
 }
 
