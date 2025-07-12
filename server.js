8160b1f838b307e770b060715c08438e662b1de3
Rafael Torok <rafaeltorok@gmail.com>
2025-07-11 21:38:47 -0300

    Fixed missing import library for path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 3000;
+const path = require('path');
 const filename = './gpuData.json';
 
 // Serve static files from the "public" directory
